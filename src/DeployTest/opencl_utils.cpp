36681f2a1ab702a905c777dd609b5da1cdaf6503
Sam Serrels <doogly1@gmail.com>
2015-09-01 13:34:24 +0100

    Working on device selection

--- src/DeployTest/opencl_utils.cpp
+++ src/DeployTest/opencl_utils.cpp
@@ -1,12 +1,79 @@
 #include "opencl_utils.h"
 #include <math.h>
+#include <algorithm>
+#include <functional>
+#include <assert.h>
+using namespace std;
 
 namespace ocl {
 int global_variable = 66;
+int total_Devices = 0;
+vector<cl_platform_id> cl_platforms;
+vector<device> devices;
+const uint8_t Init() {
+  total_Devices = 0;
+  return 0;
+}
 
-const void Init() {}
+// return an array of cl_device_id ordered by speed
+const uint8_t GetFastestDevices(std::vector<device> &fastdevices) {
+  if (total_Devices < 1) {
+    return 1;
+  }
+  fastdevices = devices;
+  // get fastest devices, for now order by compliation units
+  std::sort(fastdevices.begin(), fastdevices.end(),
+            [](device const &a, device const &b) { return a.computeUnits < b.computeUnits; });
+  return 0;
+}
 
-const std::string DeviceTypeString(cl_device_type type) {
+const uint8_t GetRecommendedDevices(const uint8_t count, std::vector<device> &devices) {
+  if (total_Devices < 1) {
+    return 1;
+  }
+  if (total_Devices < count) {
+		return GetRecommendedDevices(total_Devices, devices);
+  }
+  vector<device> fastdevices;
+  GetFastestDevices(fastdevices);
+  if (count == 1) {
+	  devices.push_back(fastdevices[0]);
+	  return 0;
+  }
+
+  //try to find a pair
+  //split into platforms
+  vector<cl_platform_id> suitable_p;
+  uint8_t d_max;
+  for (auto p : cl_platforms){
+	  cl_uint num_devices;
+	  cl_int status = clGetDeviceIDs(p, CL_DEVICE_TYPE_ALL, 0, nullptr, &num_devices);
+	  assert(status != CL_SUCCESS);
+	  d_max = max(d_max, (uint8_t)num_devices);
+	  if (num_devices >= count){
+		  suitable_p.push_back(p);
+	  }
+  }
+  if (suitable_p.size() == 0){
+	  //no platforms have count many Devices
+	  return GetRecommendedDevices(d_max, devices);
+  }
+
+  uint16_t i = 0;
+  for (auto dev : fastdevices){
+	  for (uint8_t j = 1; j < count; j++)
+	  {
+		  cl_uint num_devices;
+		  cl_int status = clGetDeviceIDs(cl_platforms[0], CL_DEVICE_TYPE_GPU, 0, nullptr, &num_devices);
+		  assert(status != CL_SUCCESS);
+	  }
+	  ++i;
+  }
+
+  return 0;
+}
+
+const std::string DeviceTypeString(const cl_device_type type) {
   int ss = type;
   std::string s = "";
   if (type & CL_DEVICE_TYPE_CPU) {
@@ -25,7 +92,7 @@ const std::string DeviceTypeString(cl_device_type type) {
 }
 }
 
-std::string readable_fs(unsigned int sz /*in bytes*/) {
+const std::string readable_fs(const unsigned int sz /*in bytes*/) {
   float size = (float)sz;
   unsigned int kb = 1024;
   unsigned int mb = kb * 1024;
--- src/DeployTest/opencl_utils.h
+++ src/DeployTest/opencl_utils.h
@@ -1,10 +1,20 @@
+#include <stdint.h>
 #include <string>
+#include <vector>
 #include <CL/opencl.h>
 
 namespace ocl {
+	struct device{
+		cl_device_id id;
+		uint16_t computeUnits;
+		cl_platform_id platform;
+	};
+
 extern int global_variable; /* Declaration of the variable */
-const void Init();
-const std::string DeviceTypeString(cl_device_type type);
+const uint8_t Init();
+const std::string DeviceTypeString(const cl_device_type type);
+const uint8_t GetRecommendedDevices(const uint8_t count, std::vector<device> &devices);
+extern cl_uint num_devices;
 }
 
-std::string readable_fs(unsigned int size);
+const std::string readable_fs(const unsigned int size);
