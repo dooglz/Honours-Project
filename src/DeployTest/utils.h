95b88669ba89db53069a04dc0ec87aa45e63cb3c
Sam Serrels <doogly1@gmail.com>
2015-09-02 16:24:46 +0100

    Fixing linux build

--- src/DeployTest/utils.h
+++ src/DeployTest/utils.h
@@ -11,9 +11,3 @@ static const char Spinner(uint8_t t) {
   };
   return (spinners[t % 4]);
 }
-
-static const char Loading(uint8_t t, uint8_t c) {
-  char spinners[] = {'◐', '◓', '◑',  '◒', '|', '/', '-', '\\', '.', 'o',
-                     'O', '0', '\'', '-', '.', '-', '◢', '◣',  '◤', '◥'};
-  return (spinners[(c % 4) * 4 + (t % 4)]);
-}
