4f52a44baac53b2798751e5f962b824ac51d3594
Sam Serrels <doogly1@gmail.com>
2015-11-08 14:59:32 +0000

    Cuda sort working

--- src/DeployTest/cudaSort.cpp
+++ src/DeployTest/cudaSort.cpp
@@ -13,11 +13,11 @@
 #include <cuda_runtime.h>
 
 void my_cuda_func(dim3 a, dim3 b, char *ab, int *bd);
-void Runbitonic_sort_step(dim3 a, dim3 b, float *dev_values, int j, int k;
-void RunSortKernel(dim3 blocks, dim3 threads, int *theArray, const unsigned int stage,
+void Runbitonic_sort_step(dim3 a, dim3 b, cudaStream_t stream, unsigned int *dev_values, int j, int k);
+void RunSortKernel(dim3 blocks, dim3 threads, cudaStream_t stream, int *theArray, const unsigned int stage,
   const unsigned int passOfStage, const unsigned int width);
 
-#define DEFAULTPOWER 18
+#define DEFAULTPOWER 6
 #define VERIFY true
 CudaSort::CudaSort() : Experiment(1, 4, "CudaSort", "Sorts Things") {}
 
@@ -39,10 +39,11 @@ void CudaSort::Shutdown() {}
 const int N = 16;
 const int blocksize = 16;
 const int threadsperblock = 512;
-uint32_t mxount =0;
+uint32_t mxount = 0;
 void CudaSort::Start(unsigned int num_runs, const std::vector<int> options) {
   cout << "\n cuda Sort\n";
-  const int GPU_N = cuda::total_num_devices;
+  //const int GPU_N = cuda::total_num_devices;
+  const int GPU_N =2;
   uint32_t maxN = 1 << DEFAULTPOWER;
   uint32_t maxNPC = (uint32_t)floor(maxN / GPU_N);
   size_t sz = maxN * sizeof(uint32_t);
@@ -86,11 +87,11 @@ void CudaSort::Start(unsigned int num_runs, const std::vector<int> options) {
   for (auto i = 0; i < GPU_N; i++) {
     cudaStreamSynchronize(streams[i]);
   }
-  //args emulation
-  int ** arg_inputArray = new int*[GPU_N];
-  unsigned int * arg_stage = new unsigned int[GPU_N];
-  unsigned int * arg_passOfStage = new unsigned int[GPU_N];
-  unsigned int * arg_width = new unsigned int[GPU_N];
+  // args emulation
+  int **arg_inputArray = new int *[GPU_N];
+  unsigned int *arg_stage = new unsigned int[GPU_N];
+  unsigned int *arg_passOfStage = new unsigned int[GPU_N];
+  unsigned int *arg_width = new unsigned int[GPU_N];
 
   /*
   * 2^numStages should be equal to length.
@@ -105,189 +106,192 @@ void CudaSort::Start(unsigned int num_runs, const std::vector<int> options) {
   // run the sort.
   size_t nThreads[1];
   nThreads[0] = maxNPC / (2 * 4);
-
   unsigned int swapcount = 0;
-  if (GPU_N == 2) {
-    for (uint32_t swapsize = maxNPC / 2; swapsize > 0; swapsize /= 2) {
-      for (uint32_t stage = 0; stage < numStages; stage++) {
-        // stage of the algorithm
-        for (size_t i = 0; i < GPU_N; i++) {
-          arg_stage[i] = stage;
-        }
+  for (cl_uint swapsize = maxNPC / 2; swapsize > 0; swapsize /= 2) {
 
-        // Every stage has stage + 1 passes
-        for (int passOfStage = stage; passOfStage >= 0; passOfStage--) {
-          for (size_t i = 0; i < GPU_N; i++) {
-            arg_passOfStage[i] = passOfStage;
-          }
+    unsigned int threadsperBlock = cuda::getBlockCount(threadsperblock, maxNPC);
 
-          size_t global_work_size[1] = {passOfStage ? nThreads[0] : nThreads[0] << 1};
-          for (size_t i = 0; i < GPU_N; i++) {
-            unsigned int threadsperBlock = cuda::getBlockCount(threadsperblock, global_work_size[0]);
-            unsigned int blocks = global_work_size[0] / threadsperBlock;
-            checkCudaErrors(cudaSetDevice(i));
-            dim3 a( threadsperBlock, 1);
-            dim3 b( blocks,1);
-            RunSortKernel(b, a, (int*)inBuffers[i], arg_stage[i], arg_passOfStage[i], maxNPC);
-            getLastCudaError("reduceKernel() execution failed.\n");
+    dim3 blocks((maxNPC / threadsperBlock), 1);
+    dim3 threads(threadsperBlock, 1);
 
-
-          }
-          for (auto i = 0; i < GPU_N; i++) {
-            cudaStreamSynchronize(streams[i]);
-            getLastCudaError("reduceKernel() execution failed.\n");
-          }
+    for (size_t i = 0; i < GPU_N; i++) {
+      checkCudaErrors(cudaSetDevice(i));
+      int j, k;
+      /* Major step */
+      for (k = 2; k <= maxNPC; k <<= 1) {
+        /* Minor step */
+        for (j = k >> 1; j > 0; j = j >> 1) {
+          Runbitonic_sort_step(blocks, threads, streams[i], inBuffers[i], j, k);
+          getLastCudaError("reduceKernel() execution failed.\n");
         }
       }
+    }
 
-      //read back all data
-      for (auto i = 0; i < GPU_N; i++)
-      {
-        checkCudaErrors(cudaSetDevice(i));
-        checkCudaErrors(cudaMemcpyAsync(hostBuffers[i], inBuffers[i], szPC, cudaMemcpyDeviceToHost, streams[i]));
-      }
-      for (auto i = 0; i < GPU_N; i++) {
-        cudaStreamSynchronize(streams[i]);
-      }
+    // read back all data
+    for (auto i = 0; i < GPU_N; i++) {
+      checkCudaErrors(cudaSetDevice(i));
+      checkCudaErrors(
+          cudaMemcpyAsync(hostBuffers[i], inBuffers[i], szPC, cudaMemcpyDeviceToHost, streams[i]));
+    }
+    for (auto i = 0; i < GPU_N; i++) {
+      cudaStreamSynchronize(streams[i]);
+    }
+    for (auto i = 0; i < maxNPC; i++) {
+     // cout << i << ", " << hostBuffers[0][i] << ", " << hostBuffers[1][i] << endl;
+    }
 
-      // read in the top of card 1 to temp
-      uint32_t *tmpData = new uint32_t[swapsize];
-      uint32_t a = swapsize * sizeof(uint32_t);
-      std::copy(hostBuffers[1], &hostBuffers[1][swapsize], tmpData);
+    // read in the top of card 1 to temp
+    uint32_t *tmpData = new uint32_t[swapsize];
+    uint32_t a = swapsize * sizeof(uint32_t);
+    std::copy(hostBuffers[1], &hostBuffers[1][swapsize], tmpData);
 
-      // copy bottom of card 0 to top of card 1
-      std::copy(&hostBuffers[0][(maxNPC - swapsize)], &hostBuffers[0][(maxNPC - swapsize)+a], hostBuffers[1]);
-      // write the top of card 1 to the bottom of card 0
-      std::copy(tmpData, &tmpData[swapsize], &hostBuffers[0][(maxNPC - swapsize)]);
+    // copy bottom of card 0 to top of card 1
+    std::copy(&hostBuffers[0][(maxNPC - swapsize)], &hostBuffers[0][(maxNPC - swapsize) + swapsize],
+              hostBuffers[1]);
 
-      // Copy data back to GPU
-      for (auto i = 0; i < GPU_N; i++) {
-        // Set device
-        checkCudaErrors(cudaSetDevice(i));
+    for (auto i = 0; i < maxNPC; i++) {
+    //  cout << i << ", " << hostBuffers[0][i] << ", " << hostBuffers[1][i] << endl;
+    }
+    // write the top of card 1 to the bottom of card 0
+    std::copy(tmpData, &tmpData[swapsize], &hostBuffers[0][(maxNPC - swapsize)]);
 
-        // Copy input data from CPU
-        checkCudaErrors(
-          cudaMemcpyAsync(inBuffers[i], hostBuffers[i], szPC, cudaMemcpyHostToDevice, streams[i]));
-      }
-      // wait
-      for (auto i = 0; i < GPU_N; i++) {
-        cudaStreamSynchronize(streams[i]);
-      }
+    for (auto i = 0; i < maxNPC; i++) {
+     // cout << i << ", " << hostBuffers[0][i] << ", " << hostBuffers[1][i] << endl;
+    }
+    // Copy data back to GPU
+    for (auto i = 0; i < GPU_N; i++) {
+      // Set device
+      checkCudaErrors(cudaSetDevice(i));
 
-
-      delete[] tmpData;
-      ++swapcount;
+      // Copy input data from CPU
+      checkCudaErrors(cudaMemcpy(inBuffers[i], hostBuffers[i], szPC, cudaMemcpyHostToDevice));
+      cudaDeviceSynchronize();
+    }
+    // wait
+    for (auto i = 0; i < GPU_N; i++) {
+      cudaStreamSynchronize(streams[i]);
+    }
+    delete[] tmpData;
+    ++swapcount;
+    for (auto i = 0; i < maxNPC; i++) {
+     // cout << i << ", " << hostBuffers[0][i] << ", " << hostBuffers[1][i] << endl;
     }
   }
 
-  //read back all data
-  for (auto i = 0; i < GPU_N; i++)
-  {
+  // read back all data
+  for (auto i = 0; i < GPU_N; i++) {
     checkCudaErrors(cudaSetDevice(i));
     checkCudaErrors(cudaMemcpyAsync(hostBuffers[i], inBuffers[i], szPC, cudaMemcpyDeviceToHost, streams[i]));
   }
   for (auto i = 0; i < GPU_N; i++) {
     cudaStreamSynchronize(streams[i]);
   }
-  cout << 22;
+  for (auto i = 0; i < maxNPC; i++) {
+     cout << i << ", " << hostBuffers[0][i] << ", " << hostBuffers[1][i] << endl;
+  }
 
-  /*
-  Timer time_sort;
-  // do one final sort, or possibly the first sort if 1 gpu
-  for (cl_int stage = 0; stage < numStages; stage++) {
-    // stage of the algorithm
-    for (size_t i = 0; i < cq.size(); i++) {
-      ret = clSetKernelArg(kernels[i], 1, sizeof(cl_uint), (void *)&stage);
-      assert(ret == CL_SUCCESS);
-    }
-    // Every stage has stage + 1 passes
-    for (cl_int passOfStage = stage; passOfStage >= 0; passOfStage--) {
-      for (size_t i = 0; i < cq.size(); i++) {
-        ret = clSetKernelArg(kernels[i], 2, sizeof(cl_uint), (void *)&passOfStage);
-        assert(ret == CL_SUCCESS);
+
+      /*
+      Timer time_sort;
+      // do one final sort, or possibly the first sort if 1 gpu
+      for (cl_int stage = 0; stage < numStages; stage++) {
+        // stage of the algorithm
+        for (size_t i = 0; i < cq.size(); i++) {
+          ret = clSetKernelArg(kernels[i], 1, sizeof(cl_uint), (void *)&stage);
+          assert(ret == CL_SUCCESS);
+        }
+        // Every stage has stage + 1 passes
+        for (cl_int passOfStage = stage; passOfStage >= 0; passOfStage--) {
+          for (size_t i = 0; i < cq.size(); i++) {
+            ret = clSetKernelArg(kernels[i], 2, sizeof(cl_uint), (void *)&passOfStage);
+            assert(ret == CL_SUCCESS);
+          }
+
+          size_t global_work_size[1] = {passOfStage ? nThreads[0] : nThreads[0] << 1};
+
+          for (size_t i = 0; i < cq.size(); i++) {
+            ret =
+                clEnqueueNDRangeKernel(cq[i], kernels[i], 1, 0, global_work_size, NULL, 0, NULL,
+    &e[i]);
+            assert(ret == CL_SUCCESS);
+          }
+          for (auto q : cq) {
+            ret = clFinish(q); // Wait untill all commands executed.
+            assert(ret == CL_SUCCESS);
+          }
+        }
       }
+      time_sort.Stop();
+      if (runs == 0) {
+        r.headdings.push_back("Sort_" + to_string(swapcount));
+      }
+      times.push_back(time_sort.Duration_NS());
+      if (cq.size() == 2) {
+        // may be a pssobility that the 2 edge values arn't in the corrct place
+        cl_uint a = 0;
+        cl_uint b = 0;
+        // last value of 0
+        ret = clEnqueueReadBuffer(cq[0], inBuffers[0], CL_TRUE, szPC - sizeof(cl_uint),
+    sizeof(cl_uint),
+                                  &a, 0, NULL, NULL);
+        assert(ret == CL_SUCCESS);
+        // fist value of 1
+        ret = clEnqueueReadBuffer(cq[1], inBuffers[1], CL_TRUE, 0, sizeof(cl_uint), &b, 0, NULL,
+    NULL);
+        assert(ret == CL_SUCCESS);
+        if (a < b) {
+          // yup, swap them round
+          ret = clEnqueueWriteBuffer(cq[0], inBuffers[0], CL_TRUE, szPC - sizeof(cl_uint),
+                                     sizeof(cl_uint), &b, 0, NULL, NULL);
+          assert(ret == CL_SUCCESS);
+          ret =
+              clEnqueueWriteBuffer(cq[1], inBuffers[1], CL_TRUE, 0, sizeof(cl_uint), &a, 0, NULL,
+    NULL);
+          assert(ret == CL_SUCCESS);
+        }
+      }
+      // stop timer here
 
-      size_t global_work_size[1] = {passOfStage ? nThreads[0] : nThreads[0] << 1};
-
+      Timer time_copyback;
+      // Copy results from the memory buffer
+      cl_uint *outData = new cl_uint[maxN];
       for (size_t i = 0; i < cq.size(); i++) {
+        cl_uint offset = (i * maxNPC);
         ret =
-            clEnqueueNDRangeKernel(cq[i], kernels[i], 1, 0, global_work_size, NULL, 0, NULL, &e[i]);
+            clEnqueueReadBuffer(cq[i], inBuffers[i], CL_TRUE, 0, szPC, &outData[offset], 0, NULL,
+    NULL);
+        assert(ret == CL_SUCCESS);
+        ret = clFinish(cq[i]); // Wait untill all commands executed.
         assert(ret == CL_SUCCESS);
       }
-      for (auto q : cq) {
-        ret = clFinish(q); // Wait untill all commands executed.
-        assert(ret == CL_SUCCESS);
+      time_copyback.Stop();
+    #ifdef VERIFY
+      assert(CheckArrayOrder(outData, maxN, false));
+    #endif
+      delete outData;
+
+      if (runs == 0) {
+        r.headdings.push_back("Copy Back");
       }
+      times.push_back(time_copyback.Duration_NS());
+      r.times.push_back(times);
+      ++runs;
     }
+    */
+      // Cleanup and shutdown
+      for (auto i = 0; i < GPU_N; i++) {
+    checkCudaErrors(cudaSetDevice(i));
+    checkCudaErrors(cudaFree(inBuffers[i]));
+    checkCudaErrors(cudaStreamDestroy(streams[i]));
+    checkCudaErrors(cudaFreeHost(hostBuffers[i]));
+    cudaDeviceReset();
   }
-  time_sort.Stop();
-  if (runs == 0) {
-    r.headdings.push_back("Sort_" + to_string(swapcount));
-  }
-  times.push_back(time_sort.Duration_NS());
-  if (cq.size() == 2) {
-    // may be a pssobility that the 2 edge values arn't in the corrct place
-    cl_uint a = 0;
-    cl_uint b = 0;
-    // last value of 0
-    ret = clEnqueueReadBuffer(cq[0], inBuffers[0], CL_TRUE, szPC - sizeof(cl_uint), sizeof(cl_uint),
-                              &a, 0, NULL, NULL);
-    assert(ret == CL_SUCCESS);
-    // fist value of 1
-    ret = clEnqueueReadBuffer(cq[1], inBuffers[1], CL_TRUE, 0, sizeof(cl_uint), &b, 0, NULL, NULL);
-    assert(ret == CL_SUCCESS);
-    if (a < b) {
-      // yup, swap them round
-      ret = clEnqueueWriteBuffer(cq[0], inBuffers[0], CL_TRUE, szPC - sizeof(cl_uint),
-                                 sizeof(cl_uint), &b, 0, NULL, NULL);
-      assert(ret == CL_SUCCESS);
-      ret =
-          clEnqueueWriteBuffer(cq[1], inBuffers[1], CL_TRUE, 0, sizeof(cl_uint), &a, 0, NULL, NULL);
-      assert(ret == CL_SUCCESS);
-    }
-  }
-  // stop timer here
 
-  Timer time_copyback;
-  // Copy results from the memory buffer
-  cl_uint *outData = new cl_uint[maxN];
-  for (size_t i = 0; i < cq.size(); i++) {
-    cl_uint offset = (i * maxNPC);
-    ret =
-        clEnqueueReadBuffer(cq[i], inBuffers[i], CL_TRUE, 0, szPC, &outData[offset], 0, NULL, NULL);
-    assert(ret == CL_SUCCESS);
-    ret = clFinish(cq[i]); // Wait untill all commands executed.
-    assert(ret == CL_SUCCESS);
-  }
-  time_copyback.Stop();
-#ifdef VERIFY
-  assert(CheckArrayOrder(outData, maxN, false));
-#endif
-  delete outData;
+  delete streams;
+  delete[] hostBuffers;
+  delete[] inBuffers;
+  delete[] rndData;
 
-  if (runs == 0) {
-    r.headdings.push_back("Copy Back");
-  }
-  times.push_back(time_copyback.Duration_NS());
-  r.times.push_back(times);
-  ++runs;
-}
-*/
-// Cleanup and shutdown
-for (auto i = 0; i < GPU_N; i++) {
-  checkCudaErrors(cudaSetDevice(i));
-  checkCudaErrors(cudaFree(inBuffers[i]));
-  checkCudaErrors(cudaStreamDestroy(streams[i]));
-  checkCudaErrors(cudaFreeHost(hostBuffers[i]));
-  cudaDeviceReset();
-}
-
-delete streams;
-delete[] hostBuffers;
-delete[] inBuffers;
-delete[] rndData;
-
-cout << "\n Sort finished\n";
-running = false;
-}
-;
+  cout << "\n Sort finished\n";
+  running = false;
+};
--- src/DeployTest/cudasortkernel.cu
+++ src/DeployTest/cudasortkernel.cu
@@ -185,7 +185,7 @@ __global__ void bitonicSort2(int *inputArray, const unsigned int stage,
   }
 }
 
-__global__ void bitonic_sort_step(float *dev_values, int j, int k)
+__global__ void bitonic_sort_step(unsigned int *dev_values, int j, int k)
 {
   unsigned int i, ixj; /* Sorting partners: i and ixj */
   i = threadIdx.x + blockDim.x * blockIdx.x;
@@ -197,7 +197,7 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
       /* Sort ascending */
       if (dev_values[i]>dev_values[ixj]) {
         /* exchange(i,ixj); */
-        float temp = dev_values[i];
+        unsigned int temp = dev_values[i];
         dev_values[i] = dev_values[ixj];
         dev_values[ixj] = temp;
       }
@@ -206,7 +206,7 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
       /* Sort descending */
       if (dev_values[i]<dev_values[ixj]) {
         /* exchange(i,ixj); */
-        float temp = dev_values[i];
+        unsigned int temp = dev_values[i];
         dev_values[i] = dev_values[ixj];
         dev_values[ixj] = temp;
       }
@@ -214,8 +214,9 @@ __global__ void bitonic_sort_step(float *dev_values, int j, int k)
   }
 }
 
-void Runbitonic_sort_step(dim3 a, dim3 b, float *dev_values, int j, int k) {
-  bitonic_sort_step << <a, b >> > (dev_values,j, k);
+void Runbitonic_sort_step(dim3 a, dim3 b, cudaStream_t stream, unsigned int *dev_values, int j, int k) {
+  bitonic_sort_step << <a, b, 0, stream >> > (dev_values, j, k);
+  cudaDeviceSynchronize();
 }
 
 
@@ -224,9 +225,9 @@ void my_cuda_func(dim3 a, dim3 b, char *ab, int *bd) {
   // cudaDeviceSynchronize();
 }
 
-void RunSortKernel(dim3 blocks, dim3 threads, int *theArray, const unsigned int stage,
+void RunSortKernel(dim3 blocks, dim3 threads, cudaStream_t stream, int *theArray, const unsigned int stage,
                    const unsigned int passOfStage, const unsigned int width) {
-  bitonicSort2<<<blocks, threads>>>(theArray, stage, passOfStage, width);
+  bitonicSort2 << <blocks, threads, 0, stream >> >(theArray, stage, passOfStage, width);
 
   // cudaDeviceSynchronize();
 }
