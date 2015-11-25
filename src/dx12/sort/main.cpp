fcf7cbbbf50c6107b8e114b857b7fdac299b6a73
Sam Serrels <doogly1@gmail.com>
2015-11-25 20:15:26 +0000

    2 cards transferring dats with dx12!

--- src/dx12/sort/main.cpp
+++ src/dx12/sort/main.cpp
@@ -7,14 +7,14 @@ using Microsoft::WRL::ComPtr;
 //#define NSIZE 64
 //#define NSIZE 128
 //#define NSIZE 256
-//#define NSIZE 1024
+#define NSIZE 1024
 //#define NSIZE 2048
 //#define NSIZE 16384
 //#define NSIZE 32768
 //#define NSIZE 65536
 //#define NSIZE 131072
 //#define NSIZE 1048576
-#define NSIZE 4194304
+//#define NSIZE 4194304
 #define NSIZEBYTES NSIZE * 4
 #define CARDS 2
 #define NSIZEPC NSIZE / CARDS
@@ -40,15 +40,21 @@ struct ConstantBufferCS {
 };
 const UINT CBSIZE = sizeof(ConstantBufferCS);
 
+struct constantBufStuff {
+  UINT8 *bufferData;
+  ComPtr<ID3D12Resource> buffer;
+  ComPtr<ID3D12Resource> bufferUpload;
+};
 
-
-void ExecuteAndWait(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12CommandQueue> cmdQueue,
-                    ComPtr<ID3D12CommandAllocator> cmdAlloc, ComPtr<ID3D12PipelineState> pso, ComPtr<ID3D12Device> device) {
+void Execute(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12CommandQueue> cmdQueue) {
   // Execute
   ThrowIfFailed(cmdList->Close());
   ID3D12CommandList *cmds = cmdList.Get();
   cmdQueue->ExecuteCommandLists(1, &cmds);
-
+}
+void Wait(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12CommandQueue> cmdQueue,
+          ComPtr<ID3D12CommandAllocator> cmdAlloc, ComPtr<ID3D12PipelineState> pso,
+          ComPtr<ID3D12Device> device) {
   ComPtr<ID3D12Fence> m_fence;
   ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence)));
 
@@ -67,11 +73,14 @@ void ExecuteAndWait(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12Comm
   ThrowIfFailed(cmdAlloc->Reset());
   ThrowIfFailed(cmdList->Reset(cmdAlloc.Get(), pso.Get()));
 }
-struct constantBufStuff {
-  UINT8 *bufferData;
-  ComPtr<ID3D12Resource> buffer;
-  ComPtr<ID3D12Resource> bufferUpload;
-};
+
+void ExecuteAndWait(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12CommandQueue> cmdQueue,
+  ComPtr<ID3D12CommandAllocator> cmdAlloc, ComPtr<ID3D12PipelineState> pso,
+  ComPtr<ID3D12Device> device) {
+  Execute(cmdList, cmdQueue);
+  Wait(cmdList, cmdQueue, cmdAlloc, pso, device);
+}
+
 constantBufStuff createConstBuf(ComPtr<ID3D12Device> m_device) {
   const UINT bufferSize = sizeof(ConstantBufferCS);
   constantBufStuff cbf;
@@ -96,10 +105,11 @@ constantBufStuff createConstBuf(ComPtr<ID3D12Device> m_device) {
       &CD3DX12_RESOURCE_DESC::Buffer(bufferSize), D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
       IID_PPV_ARGS(&cbf.bufferUpload)));
 #endif
-return cbf;
+  return cbf;
 }
 
-void setConstBuf(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12Device> m_device, constantBufStuff cbf, ConstantBufferCS cb) {
+void setConstBuf(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12Device> m_device,
+                 constantBufStuff cbf, ConstantBufferCS cb) {
 
 #if USEMAPPEDUNIFORM
   memcpy(cbf.bufferData, &cb, sizeof(ConstantBufferCS));
@@ -114,8 +124,8 @@ void setConstBuf(ComPtr<ID3D12GraphicsCommandList> cmdList, ComPtr<ID3D12Device>
   computeCBData.RowPitch = CBSIZE;
   computeCBData.SlicePitch = computeCBData.RowPitch;
 
-  ThrowIfFailed(UpdateSubresources<1>(cmdList.Get(), cbf.buffer.Get(),
-    cbf.bufferUpload.Get(), 0, 0, 1, &computeCBData));
+  ThrowIfFailed(UpdateSubresources<1>(cmdList.Get(), cbf.buffer.Get(), cbf.bufferUpload.Get(), 0, 0,
+                                      1, &computeCBData));
   setResourceBarrier(cmdList.Get(), cbf.buffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST,
                      D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
 #endif
@@ -154,8 +164,8 @@ void Sort(int size, ComPtr<ID3D12GraphicsCommandList> cmdList,
   for (k = 2; k <= size; k <<= 1) {
     //  Minor step
     for (j = k >> 1; j > 0; j = j >> 1) {
-      setConstBuf(cmdList, device,cbf, {j, k, dy});
-      ExecuteAndWait(cmdList, cmdQueue, cmdAlloc, pso,device);
+      setConstBuf(cmdList, device, cbf, {j, k, dy});
+      ExecuteAndWait(cmdList, cmdQueue, cmdAlloc, pso, device);
 
       cmdList->SetComputeRootSignature(rootSignature.Get());
       cmdList->SetPipelineState(pso.Get());
@@ -237,7 +247,6 @@ void proc() {
     }
   }
 
-
   // Create shader
   ID3D10Blob *cs;
   {
@@ -247,7 +256,7 @@ void proc() {
     flag |= D3DCOMPILE_DEBUG;
 #endif /* _DEBUG */
     ThrowIfFailed(D3DCompileFromFile(L"sort.hlsl", nullptr, nullptr, "CSMain", "cs_5_0", flag, 0,
-      &cs, &info));
+                                     &cs, &info));
   }
 
   for (size_t i = 0; i < CARDS; ++i) {
@@ -291,39 +300,39 @@ void proc() {
     psoDesc.CS.BytecodeLength = cs->GetBufferSize();
     psoDesc.CS.pShaderBytecode = cs->GetBufferPointer();
     psoDesc.pRootSignature = rootSignature[i].Get();
-    ThrowIfFailed(
-      devices[i]->CreateComputePipelineState(&psoDesc, IID_PPV_ARGS(pso[i].ReleaseAndGetAddressOf())));
+    ThrowIfFailed(devices[i]->CreateComputePipelineState(
+        &psoDesc, IID_PPV_ARGS(pso[i].ReleaseAndGetAddressOf())));
 
     // Create DescriptorHeap for UAV
     D3D12_DESCRIPTOR_HEAP_DESC desc = {};
     desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
     desc.NumDescriptors = 10;
     desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
-    ThrowIfFailed(
-      devices[i]->CreateDescriptorHeap(&desc, IID_PPV_ARGS(descHeapUav[i].ReleaseAndGetAddressOf())));
+    ThrowIfFailed(devices[i]->CreateDescriptorHeap(
+        &desc, IID_PPV_ARGS(descHeapUav[i].ReleaseAndGetAddressOf())));
 
     // Create buffer on device memory
     auto resourceDesc =
-      CD3DX12_RESOURCE_DESC::Buffer(NSIZEBYTES, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS |
-        D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
+        CD3DX12_RESOURCE_DESC::Buffer(NSIZEBYTES, D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS |
+                                                      D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS);
     ThrowIfFailed(devices[i]->CreateCommittedResource(
-      &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), D3D12_HEAP_FLAG_NONE, &resourceDesc,
-      D3D12_RESOURCE_STATE_UNORDERED_ACCESS, nullptr,
-      IID_PPV_ARGS(bufferDefault[i].ReleaseAndGetAddressOf())));
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT), D3D12_HEAP_FLAG_NONE, &resourceDesc,
+        D3D12_RESOURCE_STATE_UNORDERED_ACCESS, nullptr,
+        IID_PPV_ARGS(bufferDefault[i].ReleaseAndGetAddressOf())));
     bufferDefault[i]->SetName(L"BufferDefault");
 
     // Create buffer on system memory
     resourceDesc = CD3DX12_RESOURCE_DESC::Buffer(NSIZEBYTES);
     ThrowIfFailed(devices[i]->CreateCommittedResource(
-      &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_READBACK), D3D12_HEAP_FLAG_NONE, &resourceDesc,
-      D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
-      IID_PPV_ARGS(bufferReadback[i].ReleaseAndGetAddressOf())));
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_READBACK), D3D12_HEAP_FLAG_NONE, &resourceDesc,
+        D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
+        IID_PPV_ARGS(bufferReadback[i].ReleaseAndGetAddressOf())));
     bufferReadback[i]->SetName(L"BufferReadback");
 
     ThrowIfFailed(devices[i]->CreateCommittedResource(
-      &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), D3D12_HEAP_FLAG_NONE,
-      &CD3DX12_RESOURCE_DESC::Buffer(NSIZEBYTES), D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
-      IID_PPV_ARGS(&bufferUpload[i])));
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD), D3D12_HEAP_FLAG_NONE,
+        &CD3DX12_RESOURCE_DESC::Buffer(NSIZEBYTES), D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
+        IID_PPV_ARGS(&bufferUpload[i])));
     bufferUpload[i]->SetName(L"BufferUpload");
 
     // Setup UAV
@@ -333,33 +342,105 @@ void proc() {
     uavDesc.Buffer.NumElements = NSIZE;
     uavDesc.Buffer.StructureByteStride = 4;
     devices[i]->CreateUnorderedAccessView(bufferDefault[i].Get(), nullptr, &uavDesc,
-      descHeapUav[i]->GetCPUDescriptorHandleForHeapStart());
+                                          descHeapUav[i]->GetCPUDescriptorHandleForHeapStart());
 
     cbs[i] = createConstBuf(devices[i]);
     ExecuteAndWait(cmdList[i], cmdQueue[i], cmdAlloc[i], pso[i], devices[i]);
-
   }
 
   cs->Release();
 
+#if CARDS > 1
+  ComPtr<ID3D12Resource> m_crossAdapterResource[CARDS];
+  ComPtr<ID3D12Heap> shared_heap[CARDS];
+  // Create cross-adapter shared resources on the primary adapter,
+  //  and open the shared handles on  the secondary adapter.
+  {
+    D3D12_RESOURCE_DESC crossAdapterDesc = CD3DX12_RESOURCE_DESC::Buffer(256);
+    crossAdapterDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_CROSS_ADAPTER;
+
+    CD3DX12_HEAP_DESC heapDesc(256, D3D12_HEAP_TYPE_DEFAULT, 0,
+                               D3D12_HEAP_FLAG_SHARED | D3D12_HEAP_FLAG_SHARED_CROSS_ADAPTER);
+
+
+    ThrowIfFailed(devices[0]->CreateHeap(&heapDesc, IID_PPV_ARGS(&shared_heap[0])));
+
+    HANDLE heapHandle = nullptr;
+    auto a = devices[0]->CreateSharedHandle(shared_heap[0].Get(), nullptr, GENERIC_ALL,nullptr, &heapHandle);
+
+    HRESULT openSharedHandleResult = devices[1]->OpenSharedHandle(heapHandle, IID_PPV_ARGS(&shared_heap[1]));
+
+    // We can close the handle after opening the cross-adapter shared resource.
+    CloseHandle(heapHandle);
+
+    ThrowIfFailed(openSharedHandleResult);
+
+    ThrowIfFailed(devices[0]->CreatePlacedResource(shared_heap[0].Get(), 0, &crossAdapterDesc,
+                                                   D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
+                                                   IID_PPV_ARGS(&m_crossAdapterResource[0])));
+    ThrowIfFailed(devices[1]->CreatePlacedResource(shared_heap[1].Get(), 0, &crossAdapterDesc,
+                                                   D3D12_RESOURCE_STATE_COPY_DEST, nullptr,
+                                                   IID_PPV_ARGS(&m_crossAdapterResource[1])));
+    ExecuteAndWait(cmdList[0], cmdQueue[0], cmdAlloc[0], pso[0], devices[0]);
+    ExecuteAndWait(cmdList[1], cmdQueue[1], cmdAlloc[1], pso[1], devices[1]);
+  }
+#endif
+/*
   for (size_t i = 0; i < CARDS; ++i) {
     SendRandom(cmdList[i], bufferUpload[i], bufferDefault[i]);
-    ExecuteAndWait(cmdList[i], cmdQueue[i], cmdAlloc[i], pso[i], devices[i]);
+    Execute(cmdList[i], cmdQueue[i]);
   }
-  cout << "Numbers copied " << endl;
 
   for (size_t i = 0; i < CARDS; ++i) {
+    Wait(cmdList[i], cmdQueue[i], cmdAlloc[i], pso[i], devices[i]);
+  }
+  */
+
+  SendRandom(cmdList[0], bufferUpload[0], bufferDefault[0]);
+  ExecuteAndWait(cmdList[0], cmdQueue[0], cmdAlloc[0], pso[0], devices[0]);
+  //let's do some testing 
+
+  //copy from def[0] to share [0]
+  setResourceBarrier(cmdList[0].Get(), bufferDefault[0].Get(),
+                     D3D12_RESOURCE_STATE_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_COPY_SOURCE);
+
+  cmdList[0]->CopyBufferRegion(m_crossAdapterResource[0].Get(), 0, bufferDefault[0].Get(), 0, 256);
+
+  setResourceBarrier(cmdList[0].Get(), bufferDefault[0].Get(), D3D12_RESOURCE_STATE_COPY_SOURCE,
+                     D3D12_RESOURCE_STATE_UNORDERED_ACCESS);
+
+  ExecuteAndWait(cmdList[0], cmdQueue[0], cmdAlloc[0], pso[0], devices[0]);
+
+//copy into def[1]
+  setResourceBarrier(cmdList[1].Get(), bufferDefault[1].Get(),
+    D3D12_RESOURCE_STATE_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_COPY_DEST);
+
+  setResourceBarrier(cmdList[1].Get(), m_crossAdapterResource[1].Get(), D3D12_RESOURCE_STATE_COPY_DEST,
+    D3D12_RESOURCE_STATE_COPY_SOURCE);
+
+  cmdList[1]->CopyBufferRegion(bufferDefault[1].Get(), 0, m_crossAdapterResource[1].Get(), 0, 256);
+
+  setResourceBarrier(cmdList[1].Get(), bufferDefault[1].Get(), D3D12_RESOURCE_STATE_COPY_DEST,
+    D3D12_RESOURCE_STATE_UNORDERED_ACCESS);
+
+  ExecuteAndWait(cmdList[1], cmdQueue[1], cmdAlloc[1], pso[1], devices[1]);
+
+  cout << "Numbers copied " << endl;
+
+/*
+  for (size_t i = 0; i < CARDS; ++i) {
     auto start = chrono::high_resolution_clock::now();
-    Sort(NSIZE, cmdList[i], rootSignature[i], pso[i], descHeapUav[i], devices[i], cmdQueue[i], cmdAlloc[i], cbs[i]);
+    Sort(NSIZE, cmdList[i], rootSignature[i], pso[i], descHeapUav[i], devices[i], cmdQueue[i],
+         cmdAlloc[i], cbs[i]);
     ExecuteAndWait(cmdList[i], cmdQueue[i], cmdAlloc[i], pso[i], devices[i]);
     auto end = chrono::high_resolution_clock::now();
     cout << chrono::duration_cast<chrono::nanoseconds>(end - start).count() << "ns" << endl;
   }
-
+  */
   cout << "Numbers Sorted" << endl;
   for (size_t i = 0; i < CARDS; ++i) {
-    setResourceBarrier(cmdList[i].Get(), bufferDefault[i].Get(), D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
-                       D3D12_RESOURCE_STATE_COPY_SOURCE);
+    setResourceBarrier(cmdList[i].Get(), bufferDefault[i].Get(),
+                       D3D12_RESOURCE_STATE_UNORDERED_ACCESS, D3D12_RESOURCE_STATE_COPY_SOURCE);
     ExecuteAndWait(cmdList[i], cmdQueue[i], cmdAlloc[i], pso[i], devices[i]);
 
     cmdList[i]->CopyResource(bufferReadback[i].Get(), bufferDefault[i].Get());
@@ -388,7 +469,7 @@ void proc() {
     }
     cout << endl;
 
-    CheckArrayOrder(rndData, NSIZE, true);
+    //CheckArrayOrder(rndData, NSIZE, true);
     cout << "Validation done" << endl;
 
     delete[] rndData;
