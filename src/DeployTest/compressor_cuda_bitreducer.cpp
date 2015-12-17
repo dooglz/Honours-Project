16d8a0efcd84e37becbdeb17a28473f83c39b958
Sam Serrels <doogly1@gmail.com>
2015-12-17 17:12:47 +0000

    bit reducer and sequential compactor working.

--- src/DeployTest/compressor_cuda_bitreducer.cpp
+++ src/DeployTest/compressor_cuda_bitreducer.cpp
@@ -11,7 +11,8 @@ compressor_cuda_bitreducer::~compressor_cuda_bitreducer(){}
 void compressor_cuda_bitreducer::Init(){}
 void compressor_cuda_bitreducer::Shutdown(){}
 
-void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf);
+void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, uint32_t *intBuf);
+void Run_seq_compact(const uint32_t *input_array, const uint16_t dataSize, uint32_t* sizeBuf);
 
 void compressor_cuda_bitreducer::Compress(const uint32_t* gpuBuffer, const size_t dataSize){
 
@@ -27,17 +28,36 @@ void compressor_cuda_bitreducer::Compress(const uint32_t* gpuBuffer, const size_
 	const dim3 threads(threadsperBlock, 1);
 
 	//reduce the bits
-	Run_BitReduce(blocks, threads, gpuBuffer, (uint16_t)dataSize, intBuf);
+	Run_BitReduce(blocks, threads, gpuBuffer,intBuf);
 	getLastCudaError("Run_BitReduce() execution failed.\n");
-
 	checkCudaErrors(cudaDeviceSynchronize());
+
 	//verify
 	uint32_t* hostBuf;
 	checkCudaErrors(cudaMallocHost((void**)&hostBuf, int_buf_size));
+
 	checkCudaErrors(cudaMemcpy(hostBuf, intBuf, int_buf_size, cudaMemcpyDeviceToHost));
 	checkCudaErrors(cudaDeviceSynchronize());
 
 	//compact
+	uint32_t* sizeBuf;
+	checkCudaErrors(cudaMalloc(&sizeBuf, 4));
+	Run_seq_compact(intBuf, dataSize, sizeBuf);
+	getLastCudaError("Run_seq_compact() execution failed.\n");
+	checkCudaErrors(cudaDeviceSynchronize());
+
+	uint32_t finalSize;
+	checkCudaErrors(cudaMemcpy(&finalSize, sizeBuf, 4, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaFree(sizeBuf));
+	checkCudaErrors(cudaDeviceSynchronize());
+
+	const float ratio = (float)finalSize / (float)(dataSize* sizeof(uint32_t));
+
+	memset(hostBuf, 0, int_buf_size);
+	checkCudaErrors(cudaMemcpy(hostBuf, intBuf, int_buf_size, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaDeviceSynchronize());
+
+	checkCudaErrors(cudaFreeHost(hostBuf));
 
 	checkCudaErrors(cudaFree(intBuf));
 	uint16_t qq = threadsperBlock;
--- src/DeployTest/compressor_cuda_bitreducer_kernel.cu
+++ src/DeployTest/compressor_cuda_bitreducer_kernel.cu
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-__global__ void bit_reduce(const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf)
+__global__ void bit_reduce(const uint32_t *input_array, uint32_t *intBuf)
 {
  uint32_t i = threadIdx.x + blockDim.x * blockIdx.x;
  uint32_t a = input_array[i];
@@ -22,8 +22,29 @@ __global__ void bit_reduce(const uint32_t *input_array, const uint16_t dataSize,
 
 }
 
+__global__ void seq_compact(uint8_t *intBuf, const uint16_t dataSize, uint32_t* sizeBuf)
+{
+	uint16_t readIndex = 0;
+	uint16_t writeIndex = 0;
+	for (uint16_t i = 0; i < dataSize; ++i)
+	{
+		const uint16_t readIndex = i * 4;
+		uint8_t size = intBuf[readIndex];
+		memcpy(&intBuf[writeIndex], &intBuf[readIndex], size +1);
+		writeIndex += size + 1;
+	}
+	sizeBuf[0] = writeIndex;
 
-void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf) {
-	bit_reduce << <a, b >> > (input_array, dataSize, intBuf);
-	//cudaDeviceSynchronize();
+	//zero out the rest of the buffer
+	const uint32_t int_buf_size = (dataSize* sizeof(uint32_t)) + (dataSize * sizeof(uint8_t));
+	memset(&intBuf[writeIndex], 0, int_buf_size - int_buf_size);
+}
+
+
+void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, uint32_t *intBuf) {
+	bit_reduce << <a, b >> > (input_array, intBuf);
+}
+void Run_seq_compact(const uint32_t *input_array, const uint16_t dataSize, uint32_t* sizeBuf) {
+	const dim3 blocks(1, 1);
+	seq_compact << <blocks, blocks >> > ((uint8_t *)input_array, dataSize, sizeBuf);
 }
