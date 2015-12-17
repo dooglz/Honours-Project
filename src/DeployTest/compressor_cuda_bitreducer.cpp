6255327f729162ef22090d7c2fef9112922f1356
Sam Serrels <doogly1@gmail.com>
2015-12-17 16:20:27 +0000

    cuda bit reducer first stage working.

--- src/DeployTest/compressor_cuda_bitreducer.cpp
+++ src/DeployTest/compressor_cuda_bitreducer.cpp
@@ -11,11 +11,15 @@ compressor_cuda_bitreducer::~compressor_cuda_bitreducer(){}
 void compressor_cuda_bitreducer::Init(){}
 void compressor_cuda_bitreducer::Shutdown(){}
 
-void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize);
+void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf);
 
 void compressor_cuda_bitreducer::Compress(const uint32_t* gpuBuffer, const size_t dataSize){
 
-	//create output buffer
+	//create output
+	//TODO: have this passed in as param
+	uint32_t* intBuf;
+	const size_t int_buf_size = (dataSize* sizeof(uint32_t)) + (dataSize * sizeof(uint8_t));
+	checkCudaErrors(cudaMalloc(&intBuf, int_buf_size));
 
 	//Figure out our dimensions
 	const unsigned int threadsperBlock = cuda::getBlockCount(512, dataSize);
@@ -23,11 +27,19 @@ void compressor_cuda_bitreducer::Compress(const uint32_t* gpuBuffer, const size_
 	const dim3 threads(threadsperBlock, 1);
 
 	//reduce the bits
-	Run_BitReduce(blocks, threads, gpuBuffer, (uint16_t)dataSize);
+	Run_BitReduce(blocks, threads, gpuBuffer, (uint16_t)dataSize, intBuf);
 	getLastCudaError("Run_BitReduce() execution failed.\n");
+
+	checkCudaErrors(cudaDeviceSynchronize());
+	//verify
+	uint32_t* hostBuf;
+	checkCudaErrors(cudaMallocHost((void**)&hostBuf, int_buf_size));
+	checkCudaErrors(cudaMemcpy(hostBuf, intBuf, int_buf_size, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaDeviceSynchronize());
 
 	//compact
 
+	checkCudaErrors(cudaFree(intBuf));
 	uint16_t qq = threadsperBlock;
 }
 
--- src/DeployTest/compressor_cuda_bitreducer_kernel.cu
+++ src/DeployTest/compressor_cuda_bitreducer_kernel.cu
@@ -1,13 +1,29 @@
 #include <stdint.h>
 
-__global__ void bit_reduce(const uint32_t *input_array, const uint16_t dataSize)
+__global__ void bit_reduce(const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf)
 {
- unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
+ uint32_t i = threadIdx.x + blockDim.x * blockIdx.x;
+ uint32_t a = input_array[i];
+
+ if (a <= 0xff) {
+	 intBuf[i] = 1;
+	 uint8_t b = static_cast<uint8_t>(a);
+	 memcpy((uint8_t*)(&intBuf[i]) + 1, &b, sizeof(uint8_t));
+ }
+ else if (a <= 0xffff) {
+	 intBuf[i] = sizeof(uint16_t);
+	 uint16_t s = static_cast<uint16_t>(a);
+	 memcpy((uint8_t*)(&intBuf[i]) + 1, &s, sizeof(uint16_t));
+ }
+ else {
+	 intBuf[i] = sizeof(uint32_t);
+	 memcpy((uint8_t*)(&intBuf[i]) + 1, &a, sizeof(uint32_t));
+ }
 
 }
 
 
-void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize) {
-	bit_reduce <<<a, b >>> (input_array, dataSize);
+void Run_BitReduce(const dim3 a, const dim3 b, const uint32_t *input_array, const uint16_t dataSize, uint32_t *intBuf) {
+	bit_reduce << <a, b >> > (input_array, dataSize, intBuf);
 	//cudaDeviceSynchronize();
 }
